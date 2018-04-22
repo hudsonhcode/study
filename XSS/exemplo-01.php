35e7e146540d030c890cd977a16ffef919538ce4
Hudson Rodrigues <hudson_rs@outlook.com>
2018-04-22 19:23:30 -0300

    ok

---
+++ XSS/exemplo-01.php
@@ -0,0 +1,17 @@
+<form method="post">
+	<input type="text" name="busca">
+	<button type="submit">Enviar</button>
+
+</form>
+
+<?php
+	$_POST['busca'] = '<a href="a"><strong>Oi</strong></a><script>alert("ok")</script>';
+
+	if(isset($_POST['busca'])){
+		//echo strip_tags($_POST['busca'], "<strong><a>");
+
+		echo htmlentities($_POST['busca']);
+	}
+
+
+?>
---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,9 @@
+
+<script src='https://www.google.com/recaptcha/api.js'></script>
+<form action="cadastrar.php" method="post">
+	
+	<div class="g-recaptcha" data-sitekey="6Leo1lQUAAAAAB3gjBZgaslZ9mV8Tw5CUsDhIpfS"></div>
+	<input type="email" name="inputEmail">
+	<button type="submit">Enviar</button>
+
+</form>
